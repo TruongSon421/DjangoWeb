4e981e7a9d3c692f5443210b0ecdf3e4273b589f
hason <tranhasonpy@gmail.com>
2024-03-19 21:45:20 +0700

    add mysql

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hospital_website',
+        'USER': 'root',
+        'PASSWORD': 'password123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
